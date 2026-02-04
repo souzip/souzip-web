12564d07bb6c307555ee1309662ac2e970a705fa
Develop-KIM <kimdonghwan913@gmail.com>
2026-02-04 09:53:55 +0900

    chore: 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
+  future: {
+    compatibilityVersion: 3,
+  },
+
   devtools: { enabled: true },
   
   modules: [
