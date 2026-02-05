da9caa21ac16d3ccc1b9498efe2432675a20d7a5
Develop-KIM <kimdonghwan913@gmail.com>
2026-02-05 13:31:52 +0900

    refactor: 푸터 수정

--- pages/index.vue
+++ pages/index.vue
@@ -168,7 +168,7 @@
               </p>
               <div class="flex items-center font-medium text-sm" style="color: #FF6640;">
                 자세히 보기
-                <svg class="w-4 h-4 ml-1 group-hover:translate-x-1 transition-transform" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
                 </svg>
               </div>
